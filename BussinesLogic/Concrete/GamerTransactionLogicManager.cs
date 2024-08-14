913fbef107dc21f2d813c1f8bb97a7eab952005b
Uğur GÜLER <gulerugur190@gmail.com>
2024-08-14 20:08:47 +0300

    Busines Logic is created

---
+++ BussinesLogic/Concrete/GamerTransactionLogicManager.cs
@@ -0,0 +1,18 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLogic
+{
+    public class GamerTransactionLogicManager : GamerTransactionLogicService
+    {
+        public bool AddGamerToSystem(Gamer gamer)
+        {
+            Console.WriteLine(gamer.Name + " is real person");
+            return true;
+        }
+    }
+}
